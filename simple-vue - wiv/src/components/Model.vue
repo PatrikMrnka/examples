37b05d6f91cada719a33193cb6c997f86732b9ba
Ansoni <antoniogviegas@hotmail.com>
2022-05-24 14:10:22 +0200

    test

--- simple-vue - wiv/src/components/Model.vue
+++ simple-vue - wiv/src/components/Model.vue
@@ -11,6 +11,16 @@
 
 <script>
 import { IfcViewerAPI } from 'web-ifc-viewer';
+import {IFCWALL,
+  IFCWALLSTANDARDCASE,
+  IFCSLAB,
+  IFCWINDOW,
+  IFCCURTAINWALL,
+  IFCFURNISHINGELEMENT,
+  IFCDOOR,
+    IFCMEMBER,
+    IFCPLATE
+} from 'web-ifc';
 
 export default {
     name: 'Model',
@@ -35,9 +45,47 @@ export default {
       input.addEventListener("change",
 
           async (changed) => {
-            const file = changed.target.files[0];
-            const ifcURL = URL.createObjectURL(file);
-            await viewer.IFC.loadIfcUrl(ifcURL);
+            const file = event.target.files[0];
+            const url = URL.createObjectURL(file);
+
+            // Export to glTF and JSON
+            const result = await viewer.GLTF.exportIfcFileAsGltf({
+              ifcFileUrl: url,
+              splitByFloors: true,
+              categories: {
+                walls: [IFCWALL, IFCWALLSTANDARDCASE],
+                slabs: [IFCSLAB],
+                windows: [IFCWINDOW],
+                curtainwalls: [IFCMEMBER, IFCPLATE, IFCCURTAINWALL],
+                furniture: [IFCFURNISHINGELEMENT],
+                doors: [IFCDOOR]
+              },
+              getProperties: true
+            });
+
+            // Download result
+            const link = document.createElement('a');
+            document.body.appendChild(link);
+
+            for(const categoryName in result.gltf) {
+              const category = result.gltf[categoryName];
+              for(const levelName in category) {
+                const file = category[levelName].file;
+                if(file) {
+                  link.download = `${file.name}_${categoryName}_${levelName}.gltf`;
+                  link.href = URL.createObjectURL(file);
+                  link.click();
+                }
+              }
+            }
+
+            for(let jsonFile of result.json) {
+              link.download = `${jsonFile.name}.json`;
+              link.href = URL.createObjectURL(jsonFile);
+              link.click();
+            }
+
+            link.remove();
           },
 
           false
